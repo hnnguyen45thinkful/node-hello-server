6e0ad1b692e5bce42735f0a6d7d72b8ab1d55786
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-06 19:30:28 -0800

    updated files and server

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.get('/', function(request, response) {
     response.send("Hello World");
 });
 
-app.listen(process.env.PORT || 8080, process.env.IP);
+app.listen(process.env.PORT || 8080, function(){
+	console.log('Server is running at http://localhost:8080');
+});
