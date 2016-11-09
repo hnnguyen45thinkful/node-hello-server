5c348ccf3797bba5ef7372ea7ee80a4a7897aad8
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-08 22:34:26 -0800

    added example and did exercise jedi name works

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+//Created and modified by Hieu Nguyen
 var express = require('express');
 
 var app = express();
 
 app.get('/', function(request, response) {
     response.send("Hello World");
+});
+
+//Example from node curriculum.
+app.get('/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname;
+    var last = request.params.lastname;
+    response.send(["Hello", first, last].join(" "));
+});
+
+//Try it exercise jedi name Unit 2 Lesson 1-1
+app.get('/jedi/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname;
+    var last = request.params.lastname;
+    var jediName = last.slice(0,3) + first.slice(0,2);
+    response.send(["Hello", jediName].join(" "));
 });
 
 app.listen(process.env.PORT || 8080, function(){
